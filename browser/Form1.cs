8b4b2d6412a65397ba69950b2bd0edf47ffdded2
Dmitry Lisitsyn <Dimchik.lis@mail.ru>
2021-04-19 23:56:27 +0300

    history

--- browser/Form1.cs
+++ browser/Form1.cs
@@ -12,6 +12,7 @@ using MySqlConnector;
 using EasyTabs;
 using CefSharp.WinForms;
 using System.Windows.Forms;
+using System.IO;
 
 namespace browser
 {
@@ -27,6 +28,8 @@ namespace browser
 
         }
         public ChromiumWebBrowser chromeBrowser;
+        private string currentPath;
+        private bool writeFlag = true;
 
         public Form1()
         {
@@ -36,14 +39,30 @@ namespace browser
             InitializeChromium();
            
             toolStrip2.ImageScalingSize = new Size(30, 30);
-           
 
+            currentPath = Directory.GetCurrentDirectory().ToString();
             toolStrip2.SizeChanged += ToolStrip2_SizeChanged;
             this.SizeChanged += Form1_SizeChanged;
             chromeBrowser.TitleChanged += ChromeBrowser_TitleChanged;
             chromeBrowser.AddressChanged += ChromeBrowser_AddressChanged;
+            chromeBrowser.LoadingStateChanged += ChromeBrowser_LoadingStateChanged;
             textUrl.Click += TextUrl_Click;
-            textUrl.KeyUp += TextUrl_KeyUp;    
+            textUrl.KeyUp += TextUrl_KeyUp;
+
+            if (!File.Exists(currentPath + @"\history.txt"))
+            {
+                File.Create(currentPath + @"\history.txt");
+
+            }
+
+
+        }
+
+        private void ChromeBrowser_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            if (!e.IsLoading)
+                write_history(chromeBrowser.Address.ToString());
+
         }
 
         private void TextUrl_Click(object sender, EventArgs e)
@@ -70,6 +89,7 @@ namespace browser
             }
 
             LoadUrl(textUrl.Text);
+            
         }
 
   
@@ -79,22 +99,27 @@ namespace browser
             {
               
                chromeBrowser.Load(url);
+            
             }
             else
             {
                 chromeBrowser.Load("https://www.google.com/search?q=" + url);
+               
             }
         }
 
         private void ChromeBrowser_TitleChanged(object sender, TitleChangedEventArgs e)
         {
             this.InvokeOnUiThreadIfRequired(() => Text = e.Title);
+            
         }
 
 
         private void ChromeBrowser_AddressChanged(object sender, AddressChangedEventArgs e)
-        {
+        { 
             this.InvokeOnUiThreadIfRequired(() => textUrl.Text = e.Address);
+         
+                
         }
 
         private void Form1_SizeChanged(object sender, EventArgs e)
@@ -126,17 +151,30 @@ namespace browser
         private void buSearch_Click(object sender, EventArgs e)
         {
            LoadUrl(textUrl.Text);
+         
+
+        }
+
+        private void write_history(string url) {
+
+            using (StreamWriter sw = new StreamWriter(currentPath + @"\history.txt", true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(url);
+                sw.Close();
+            }
+
         }
 
         private void открытьФайлToolStripMenuItem_Click(object sender, EventArgs e)
         {
             OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Filter = "Office Files|*.pdf;*.txt";
+            dialog.Filter = "Office Files|*.pdf;*.txt;*.html";
             if (dialog.ShowDialog() == DialogResult.OK)
             {
                 textUrl.Text = dialog.FileName;
                 chromeBrowser.Load(textUrl.Text);
             }
+            write_history(textUrl.Text);
         }
 
         private void buBack_Click_1(object sender, EventArgs e)
